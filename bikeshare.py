017d8d41fdb846a5f5e93bb8c0adf8a388b22ea6
Celio Joao Mavume <mavume.electronico@gmail.com>
2023-01-01 17:28:54 +0200

    refactor: Added a comment line in the end of code

--- bikeshare.py
+++ bikeshare.py
@@ -283,7 +283,7 @@ def main():
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
-
+ "" Code righten by mavume""
 
 if __name__ == "__main__":
 	main()
